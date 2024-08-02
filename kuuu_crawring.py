5919c7cca8d414836131edd9ef01ce0bfbe4c534
kuuuuuuuuu <138425915+Kuuuuuuuuuuu@users.noreply.github.com>
2024-08-02 17:12:43 +0900

    Update kuuu_crawring.py_20240802
    디시인사이드 공인인증서 갤러리 1~9페이지까지 게시물 제목 크롤링

--- kuuu_crawring.py
+++ kuuu_crawring.py
@@ -1,40 +1,44 @@
-# moduel import
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = 'https://gall.dcinside.com/board/lists/'
+BASE_URL = 'https://gall.dcinside.com/mgallery/board/lists/'
 
-para = {'id' : 'iamsolo',}
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+}
 
-header = {"User-Agent":'}
+for pages in range (1,10):
+    params = {
+        'id': 'kica',
+        'page' : str(pages)
+        }
 
-resp = requests.get(BASE_URL, params=para, headers=header)
+    resp = requests.get(BASE_URL, params=params, headers=headers)
 
-# 상태 코드 출력
-#print("Status Code:", resp.status_code) # 200
+    # 상태 코드 출력
+    #print("Status Code:", resp.status_code) # 200
 
-# 요청 URL 확인
-print("Requested URL:", resp.url)
+    # 요청 URL 확인
+    #print("Requested URL:", resp.url)
 
-# 응답 내용 확인
-#print("Response Content:", resp.content)
-soup = BeautifulSoup(resp.content, 'html.parser')
-#print(soup)
+    # 응답 내용 확인
+    #print("Response Content:", resp.content)
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    #print(soup)
 
-contents = soup.find('article').find_all('a')
-#print(contents)
+    contents = soup.find('tbody').find_all('td', class_='gall_tit ub-word')
+    #print(contents)
 
-'''
-Status Code: 200
-Requested URL: https://gall.dcinside.com/board/lists/?id=iamsolo
+    '''
+    Status Code: 200
+    Requested URL: https://gall.dcinside.com/board/lists/?id=iamsolo
 
-'''
-
-
-for i in contents:
-    #print('-'*15)
-    if i.get('href'):
-        title_tag =  i.get_text(strip=True)
-        if title_tag:
-            print(title_tag)
-   # print(i)
+    '''
+    
+    for i in contents:
+          #print('-'*15)
+          if i.find('a'):
+                title_tag = i.get_text(strip=True)
+                if title_tag:
+                     print(title_tag)
+       # print(i)'''
