c6f2b708968b6575a3fa3207b9cc6ad539c62ff4
kuuuuuuuuu <138425915+Kuuuuuuuuuuu@users.noreply.github.com>
2024-08-06 17:39:25 +0900

    Update kuuu_crawring.py_20240806
    디시인사이드에서 검색어가 포함된 게시물 제목 크롤링(현재 검색어 1열만 가능, 추후 페이지 별로 검색결과 수집해내 1회 크롤링 시, 10,000개 확보 예정

--- kuuu_crawring.py
+++ kuuu_crawring.py
@@ -1,44 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-BASE_URL = 'https://gall.dcinside.com/mgallery/board/lists/'
+BASE_URL = 'https://gall.dcinside.com/board/lists/'
 
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
 }
 
-for pages in range (1,10):
-    params = {
-        'id': 'kica',
-        'page' : str(pages)
-        }
+# 세션을 초기화하여 쿠키를 관리합니다.
+session = requests.Session()
 
-    resp = requests.get(BASE_URL, params=params, headers=headers)
 
-    # 상태 코드 출력
-    #print("Status Code:", resp.status_code) # 200
+params = {
+        'id': 'baseball_new11',
+        's_type': 'search_subject',
+        's_keyword': '원하는 검색어',
+       # 'page': str(pages)
+}
 
-    # 요청 URL 확인
-    #print("Requested URL:", resp.url)
+resp = session.get(BASE_URL, params=params, headers=headers)
 
-    # 응답 내용 확인
-    #print("Response Content:", resp.content)
-    soup = BeautifulSoup(resp.content, 'html.parser')
-    #print(soup)
+# 세션을 사용하여 요청을 보냅니다.
+# 상태 코드 출력
+# 응답 내용 확인
+#print("Status Code:", resp.status_code)  # 200
+#print("Requested URL:", resp.url)  # 요청한 URL 출력
+soup = BeautifulSoup(resp.text, 'html.parser')
+#print(soup)
+# 모든 tbody 태그 찾기
+#contents = soup.find("tbody")
+title = soup.select_one("#kakao_seach_list > tbody")
+posts = soup.select(".ub-content > .gall_tit > a")  # 게시글 제목 링크 추출
+'''
+for post in posts:
+    # 제목에서 텍스트 추출
+    text = post.get_text(strip=True)
+    # 정규식을 사용하여 앞의 넘버링과 뒤의 갤러리명, 날짜 제거
+    clean_text = re.sub(r"^\d+|\w+/\d+/\d+$", "", text).strip()
+    print(clean_text)
+'''
+#print(title.text)
 
-    contents = soup.find('tbody').find_all('td', class_='gall_tit ub-word')
-    #print(contents)
+titles = [a.get_text(strip=True) for a in soup.select('td.gall_tit > a')]
 
-    '''
-    Status Code: 200
-    Requested URL: https://gall.dcinside.com/board/lists/?id=iamsolo
+# 추출된 제목 출력
+for title in titles:
+    print(title)
 
-    '''
-    
-    for i in contents:
-          #print('-'*15)
-          if i.find('a'):
-                title_tag = i.get_text(strip=True)
-                if title_tag:
-                     print(title_tag)
-       # print(i)'''
+# 참조 https://conanmoon.medium.com/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EC%9D%BC%EA%B8%B0-web-crawling-%EC%9B%B9-%ED%81%AC%EB%A1%A4%EB%A7%81-a583a852431e
